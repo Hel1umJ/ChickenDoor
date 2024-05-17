2fc2f7330e6415ac80857ba5c0fd3722429ebcfa
Brendan Pfahl <brendanisaboss2000@gmail.com>
2024-05-17 13:47:33 -0400

    testing motor directions

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Dependencies: wiringPi
+-Dependencies: wiringPi
 
-Install this on your raspberry pi so the .so files are available for use by your executable.
+-Install Dependencies on raspberry pi so the .so files are available for use by your executable.
 
 
 -Install instructions:
@@ -8,13 +8,15 @@ https://learn.sparkfun.com/tutorials/raspberry-gpio/c-wiringpi-setup
 
 1. Clone repo: https://github.com/WiringPi/WiringPi
 2. Run build script (./build); this installs libs in default lib directories on system.
-3. Compile lib into projects using -lwiringPi
+3. Link lib into projects using -lwiringPi
 
-Flash raspberry pi OS: https://ubuntu.com/tutorials/how-to-install-ubuntu-desktop-on-raspberry-pi-4#2-prepare-the-sd-card
+-Upload raspberry pi OS: https://ubuntu.com/tutorials/how-to-install-ubuntu-desktop-on-raspberry-pi-4#2-prepare-the-sd-card
 
-Headless raspberry pi setup: https://www.tomshardware.com/reviews/raspberry-pi-headless-setup-how-to,6028.html 
+-Headless raspberry pi setup: https://www.tomshardware.com/reviews/raspberry-pi-headless-setup-how-to,6028.html 
 
-raspberry pi doc + pinout: https://www.raspberrypi.com/documentation/computers/raspberry-pi.html
+-raspberry pi doc + pinout: https://www.raspberrypi.com/documentation/computers/raspberry-pi.html
+
+-wiringPi pinout: https://pinout.xyz/pinout/wiringpi
 
 
 
--- src/chickenDoor.c
+++ src/chickenDoor.c
@@ -1,22 +1,112 @@
 #include <stdio.h>
 #include <wiringPi.h>
+#include <time.h>
 
-// Pin number declaration (WiringPi scheme)
-#define LED_PIN 0  // GPIO 17 in WiringPi is pin 0
+
+/*
+*Config variables.
+*/
+#define DLST 1 //Daylight savings time; manually update this when needed.
+float openTime = 6;//Decima number from [0,24], decimal for partial hours
+float closingTime = 20; //Decima number from [0,24], decimal for partial hours
+
+/*
+*Pinout declarations
+*/
+#define SPEEDPIN 1
+#define DIR1 0  // GPIO 17 in WiringPi is pin 0; 
+#define DIR2 1
+
+/*
+*Prototypes
+*/
+//Returns the number of seconds elapsed over the current day.
+long int daySeconds();
+//Set the motor controller pins to open the door.
+void open();
+//set the motor controller direction pins to close the door.
+void close();
 
 int main(void) {
-    // Setup stuff:
+    //Initialize WiringPi and pin modes + outputs
     wiringPiSetup();  // Initialize wiringPi
-    pinMode(LED_PIN, OUTPUT);  // Set pin as output
+    pinMode(SPEEDPIN, OUTPUT);  // Set pin as output
+    pinMode(DIR1, OUTPUT);
+    pinMode(DIR2, OUTPUT);
+    analogWrite(SPEEDPIN,255);
 
-    while(1) {
-        digitalWrite(LED_PIN, HIGH);  // Turn LED on
-        delay(500);  // Wait 500ms
-        digitalWrite(LED_PIN, LOW);  // Turn LED off
-        delay(500);  // Wait 500ms
+    open();
+    for(;;){
+
     }
 
+    
+
+    // while(1) {
+    //     digitalWrite(LED_PIN, HIGH);  // Turn LED on
+    //     delay(500);  // Wait 500ms
+    //     digitalWrite(LED_PIN, LOW);  // Turn LED off
+    //     delay(500);  // Wait 500ms
+    // }
+
+
+    long int openSeconds = openTime*60*60;
+    long int closingSeconds = closingTime*60*60;
+
+    // for(;;){
+
+    //     if(daySeconds() >= openSeconds && daySeconds <= closingSeconds){
+    //         open();
+    //     }else{
+    //         close();
+    //     }
+    // }
+
+
     return 0;
+
+}
+
+/*
+*Returns the number of seconds that have elapsed in the current day.
+*/
+long int daySeconds(){
+    time_t timer;
+    struct tm y2k = {0}; //Struct holding date for Jan 1, 2000, reference point
+    long int seconds;
+    y2k.tm_hour = 0;
+    y2k.tm_min = 0;
+    y2k.tm_sec = 0;
+    y2k.tm_year = 0;
+    y2k.tm_mon = 0;
+    y2k.tm_mday = 0;
+    y2k.tm_isdst = DLST;
+    time(&timer); //get current time, same as timer = time(NULL);
+    seconds = difftime(timer,mktime(&y2k));
+
+    
+    //Note: decimal DT cannot have % done on them, must be cast to discrete numerical 
+    //type first.
+    long int secondsToday = seconds%(24*60*60);
+
+    return secondsToday;
+}
+
+void open(){
+    digitalWrite(DIR1, HIGH);
+    digitalWRite(DIR2, LOW);
+}
+
+void close(){
+    digitalWrite(DIR1, LOW);
+    digitalWRite(DIR2, HIGH);
 }
 
 
+// 1.  If currentTime > openTime && currentTime < CloseTime:
+//         Open Door
+//     Else:
+//         close door
+
+
+    
